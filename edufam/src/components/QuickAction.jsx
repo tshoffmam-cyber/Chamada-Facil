89fe465395f69e813f8f3bf8cb7abad9c040f010
tshoffmam-cyber <tshoffmam@gmail.com>
2026-06-27 13:56:00 -0300

    Create QuickAction.jsx

---
+++ edufam/src/components/QuickAction.jsx
@@ -0,0 +1,11 @@
+export default function QuickAction({ icon, title, subtitle }) {
+    return (
+          <button className="quick-action">
+                <div className="quick-icon">{icon}</div>div>
+                <div>
+                        <strong>{title}</strong>strong>
+                        <span>{subtitle}</span>span>
+                </div>div>
+          </button>button>
+        );
+}</button>
