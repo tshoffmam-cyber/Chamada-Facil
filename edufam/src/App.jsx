3e0fd2643676c59d17f1f714ea44a147b604e1b5
tshoffmam-cyber <tshoffmam@gmail.com>
2026-07-01 18:55:01 -0300

    Update App.jsx

--- edufam/src/App.jsx
+++ edufam/src/App.jsx
@@ -1,5 +1,7 @@
-import DashboardProfessor from "./pages/DashboardProfessor";
+// EduFam — src/App.jsx
+
+import DashboardProfessor from './pages/DashboardProfessor';
 
 export default function App() {
-        return <DashboardProfessor />;
+  return <DashboardProfessor />;
 }
